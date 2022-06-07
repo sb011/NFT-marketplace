e0de1739a78036582ddd288a4e93a18af430270f
Smit <smitbhoraniya11@gmail.com>
2022-06-07 14:52:32 +0530

    contract added

---
+++ contracts/NFTmarket.sol
@@ -0,0 +1,158 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+// security against transcations for multiple requests
+
+contract NFTmarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+
+    // numbers of items miniting, numbers of transactions, token that have not been sold
+    // keep track of token total number - tokenId
+    // arrays need to know the length - help to keep track for arrays
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _tokensSold;
+
+    // determine who is the owner of the contract
+    // charge a listing fees so the owner makes a commission
+
+    address payable owner;
+    uint256 listingPrice = 0.045 ether;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct MarketToken {
+        uint256 itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    mapping(uint256 => MarketToken) private idToMarketToken;
+
+    event MarketTokenMinted(
+        uint256 indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    function mintMarketItem(
+        address nftContract,
+        uint256 tokenId,
+        uint256 price
+    ) public payable nonReentrant {
+        require(price > 0, "Price must be at least one wei");
+        require(
+            msg.value == listingPrice,
+            "Price must be equal to listing price"
+        );
+
+        _tokenIds.increment();
+        uint256 itemId = _tokenIds.current();
+
+        idToMarketToken[itemId] = MarketToken(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
+
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+        emit MarketTokenMinted(
+            itemId,
+            nftContract,
+            tokenId,
+            msg.sender,
+            address(0),
+            price,
+            false
+        );
+    }
+
+    function createMarketSale(address ntfContract, uint256 itemId)
+        public
+        payable
+        nonReentrant
+    {
+        uint256 price = idToMarketToken[itemId].price;
+        uint256 tokenId = idToMarketToken[itemId].tokenId;
+        require(
+            msg.value == price,
+            "Please submit the asking price in order to continue"
+        );
+
+        idToMarketToken[itemId].seller.transfer(msg.value);
+
+        IERC721(ntfContract).transferFrom(address(this), msg.sender, tokenId);
+
+        idToMarketToken[itemId].owner = payable(msg.sender);
+        idToMarketToken[itemId].sold = true;
+        _tokensSold.increment();
+
+        payable(owner).transfer(listingPrice);
+    }
+
+    function fetchMarketToken() public view returns (MarketToken[] memory) {
+        uint256 itemCount = _tokenIds.current();
+        uint256 unsoldItemCount = _tokenIds.current() - _tokensSold.current();
+        uint256 currentIndex = 0;
+
+        MarketToken[] memory items = new MarketToken[](unsoldItemCount);
+        for (uint256 i = 0; i < itemCount; i++) {
+            if (idToMarketToken[i + 1].owner == address(0)) {
+                uint256 currentId = i + 1;
+                MarketToken storage currentItem = idToMarketToken[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    function fetchMyNFTs() public view returns (MarketToken[] memory) {
+        uint256 totalItemCount = _tokenIds.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToMarketToken[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        MarketToken[] memory items = new MarketToken[](itemCount);
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToMarketToken[i + 1].owner == msg.sender) {
+                uint256 currentId = idToMarketToken[i + 1].itemId;
+                MarketToken storage currentItem = idToMarketToken[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+}
--- contracts/nft.sol
+++ contracts/nft.sol
@@ -1,1 +1,37 @@
-//SPDX-License-Identifier:MIT
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // counters allows us to keep track of tokenIds
+    // address of marketplace for NFTs to interact
+    address contractAddress;
+
+    // OBJ: give the NFT market the ability to transact with tokens or change ownership
+    // setApprovalForAll allows us to do that with contract address
+
+    // constructor
+    constructor(address marketplaceAddress) ERC721("NFTmarketplace", "NFTZ") {
+        contractAddress = marketplaceAddress;
+    }
+
+    function mintToken(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        // passing in id and url
+        _mint(msg.sender, newItemId);
+
+        _setTokenURI(newItemId, tokenURI);
+
+        // give the marketplace the approval to transact between users
+        setApprovalForAll(contractAddress, true);
+
+        return newItemId;
+    }
+}
